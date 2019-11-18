993a952dd9c4e5638f246985a4480e3d7bb60025
KevinYuayan <yuayankevin@gmail.com>
2019-11-17 23:21:58 -0500

    added snake plane enemy

--- Assets/_Scripts/SnakePlaneController.cs
+++ Assets/_Scripts/SnakePlaneController.cs
@@ -2,16 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Util;
-
 /// <summary>
 /// File Name: Enemy1Controller.cs
 /// Author: Kevin Yuayan
 /// Last Modified by: Kevin Yuayan
-/// Date Last Modified: Oct. 2, 2019
-/// Description: Controller for the Enemy_1 prefab
+/// Date Last Modified: Nov. 17, 2019
+/// Description: Controller for the SnakePlane prefab
 /// Revision History:
 /// </summary>
-public class Enemy1Controller : CollidableObject
+public class SnakePlaneController : CollidableObject
 {
     private GameController gc;
     private AudioSource explosionSound;
@@ -19,15 +18,32 @@ public class Enemy1Controller : CollidableObject
     [Header("Speed Values")]
     [SerializeField]
     public Speed horizontalSpeedRange;
-
     [SerializeField]
     public Speed verticalSpeedRange;
+
+    public float directionDelay;
+    public float fireRate;
 
     public float verticalSpeed;
     public float horizontalSpeed;
 
     [SerializeField]
     public Boundary boundary;
+
+    [Header("GameObjects")]
+    public GameObject fire;
+    public GameObject fireSpawn;
+
+    [Header("Itm spawning")]
+    private Vector3 position;
+    private int itemChance;
+    public GameObject bonusLife;
+    public GameObject hpUp;
+    public int bonusChancePercentage;
+    public int hpUpPercentage;
+
+    private float directionTime = 0.0f;
+    private float fireTime = 0.0f;
 
     // public properties
     public override bool HasCollided
@@ -59,6 +75,7 @@ public class Enemy1Controller : CollidableObject
     {
         Move();
         CheckBounds();
+        Fire();
     }
 
     /// <summary>
@@ -66,6 +83,11 @@ public class Enemy1Controller : CollidableObject
     /// </summary>
     void Move()
     {
+        directionTime += Time.deltaTime;
+        if (directionTime >= directionDelay)
+        {
+            ChangeDirection();
+        }
         Vector2 newPosition = new Vector2(horizontalSpeed, verticalSpeed);
         Vector2 currentPosition = transform.position;
 
@@ -83,22 +105,52 @@ public class Enemy1Controller : CollidableObject
         verticalSpeed = Random.Range(verticalSpeedRange.min, verticalSpeedRange.max);
 
         float randomYPosition = Random.Range(boundary.Bottom, boundary.Top);
-        Vector2 startPosition = new Vector2(Random.Range(boundary.Right, boundary.Right + 2.0f), randomYPosition );
+        Vector2 startPosition = new Vector2(Random.Range(boundary.Right, boundary.Right + 2.0f), randomYPosition);
         transform.position = startPosition;
 
-        // Rotates the enemy to look at the direction of the path
-
-        Vector2 target = startPosition - new Vector2(horizontalSpeed, verticalSpeed);
-        var direction = target - startPosition;
-        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        LookAhead();
+    }
+    // Changes the plane's vertical speed and rotation. Plane fires a bullet after rotating
+    private void ChangeDirection()
+    {
+        directionTime = 0;
+        verticalSpeed *= -1;
+        LookAhead();
+    }
+    void Fire()
+    {
+        fireTime += Time.deltaTime;
+        if(fireTime >= fireRate)
+        {
+            GameObject fireObj;
+            fireObj = Instantiate(fire, fireSpawn.transform.position, Quaternion.identity);
+            fireObj.GetComponent<FireController>().IsEnemyBullet = true;
+            fireTime = 0;
+        }
+    }
+    // Rotates the enemy to look at the direction of the path
+    private void LookAhead()
+    {
+        Vector3 target = transform.position - new Vector3(horizontalSpeed, verticalSpeed);
+        var direction = target - transform.position;
+        float angle = (Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg) + 90;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.back);
     }
 
+
     /// <summary>
-    /// This method checks if the enemy reaches the lower boundary and then it Resets it
+    /// This method checks if the enemy reaches the left boundary and then it Resets it. Also keeps the plane in the camera
     /// </summary>
     void CheckBounds()
     {
+        if (transform.position.y > boundary.Top)
+        {
+            ChangeDirection();
+        }
+        if (transform.position.y < boundary.Bottom)
+        {
+            ChangeDirection();
+        }
         if (transform.position.x <= boundary.Left)
         {
             Reset();
@@ -113,9 +165,23 @@ public class Enemy1Controller : CollidableObject
             {
                 Destroy(this.gameObject);
                 Destroy(col.gameObject);
-                gc.Score += 50;
+                gc.Score += 100;
                 explosionSound.volume = 0.3f;
                 explosionSound.Play();
+
+                //item spawning by enemy dead
+                position = this.gameObject.transform.position;
+                itemChance = Random.Range(0, 100);
+                if (itemChance >= 0 && itemChance <= hpUpPercentage)
+                {
+                    Instantiate(hpUp, position, Quaternion.identity);
+                    Debug.Log("Hp-Up spawned");
+                }
+                if (itemChance >= 0 && itemChance <= bonusChancePercentage)
+                {
+                    Instantiate(bonusLife, position, Quaternion.identity);
+                    Debug.Log("Bonus spawned");
+                }
             }
         }
     }
